8ba35accc0bcc80128312d10f2c30022a1a6545c
Archita Racherla <archita.racherla@gmail.com>
2021-03-30 19:15:18 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -57,14 +57,15 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney-10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack-3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -83,7 +84,8 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack -3, enemyAttack);
+      playerHealth = Math.max(0,playerHealth-damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -98,6 +100,14 @@ var fight = function(enemyName) {
       }
     }
   };
+
+//function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
+};
+
+
 
   var startGame = function(){
 //reset player stats
@@ -117,7 +127,7 @@ for (var i = 0; i < enemyNames.length; i++) {
       var pickedEnemyName = enemyNames[i];
   
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
   
       // use debugger to pause script from running and check what's going on at that moment in the code
       // debugger;
